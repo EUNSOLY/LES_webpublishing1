45a8451ef8652323d83e1fb607394706e25c8dad
EUNSOLY <dmsthf9596@gmail.com>
2023-02-23 16:34:45 +0900

    23.02.23 1차평가

---
+++ js/common.js
@@ -0,0 +1,26 @@
+const $ham = document.querySelector(".ham");
+const $header = document.querySelector(".hd_wrap");
+const $tap_key = document.querySelectorAll(".tap_key");
+const $tap_img = document.querySelectorAll(".img_box > img");
+console.log($tap_key);
+console.log($tap_img);
+
+// 햄버거 버튼 토굴
+$ham.addEventListener("click", () => {
+  $header.classList.toggle("on");
+});
+
+$tap_key.forEach((btn, idx) => {
+  btn.addEventListener("click", function () {
+    $tap_key.forEach((i) => {
+      console.log(i);
+      i.classList.remove("on");
+    });
+    $tap_img.forEach((a) => {
+      a.classList.remove("on");
+      console.log(a);
+    });
+    $tap_key[idx].classList.add("on");
+    $tap_img[idx].classList.add("on");
+  });
+});
